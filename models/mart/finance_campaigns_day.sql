c3dec5ea39cc1faf0d0215e92fee05c38a069375
Daniela Rozas Barrio de Mendoza <Daarozas@hotmail.com>
2024-08-28 22:00:37 +0000

    finance_campaign_day_check

---
+++ models/mart/finance_campaigns_day.sql
@@ -0,0 +1,11 @@
+SELECT
+  campa.date_date
+  ,ROUND((fino.operational_margin-campa.ads_cost),2) AS ads_margin 
+  ,fino.average_basket
+  ,fino.operational_margin
+  ,campa.impression
+  ,campa.click
+FROM {{ref("int_campaigns_day")}} AS campa
+LEFT JOIN {{ref("finance_days")}} fino 
+  USING(date_date)
+ORDER BY date_date desc
