649d7432b874c40e4ebde2f7cbc986e3c5b42ee5
Daniela Rozas Barrio de Mendoza <Daarozas@hotmail.com>
2024-08-28 21:19:24 +0000

    pruebas_check

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,11 +1,11 @@
-SELECT *
+SELECT date_date, paid_source, campaign_key, campaign_name, ads_cost, impression, click
 FROM {{ref('stg_gz_raw_data__adwords')}}
 UNION ALL 
-SELECT *
+SELECT date_date, paid_source, campaign_key, campaign_name, ads_cost, impression, click
 FROM {{ref('stg_gz_raw_data__bing')}}
 UNION ALL
-SELECT * 
+SELECT date_date, paid_source, campaign_key, campaign_name, ads_cost, impression, click
 FROM {{ref('stg_gz_raw_data__criteo')}}
 UNION ALL 
-SELECT *
+SELECT date_date, paid_source, campaign_key, campaign_name, ads_cost, impression, click
 FROM {{ref('stg_gz_raw_data__facebook')}}
